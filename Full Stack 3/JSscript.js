43407431ec4ff5e553d2583633922b3a3da8c65e
Teddy <106557123+TeddyB0MBER@users.noreply.github.com>
2022-09-27 19:37:45 -0700

    Add files via upload

---
+++ Full Stack 3/JSscript.js
@@ -0,0 +1,32 @@
+//console.log("here I an Again")
+//var myfirstvar = "Tuong";
+//var age=36;
+// let = var
+let age = "it's over 9000";
+let userName = "goku";
+let userName2 = "vegeta";
+let vegetaDialogue = "KAKAROT!!";
+let password = "whatever";
+let searchbar = "search";
+let alert = "your on the wrong website choom"
+let hover = " Auto Zoom";
+const tax = .025;
+let youtubeIcon = "Take me Home";
+let quantity = 1-1000;
+let reccomended = "Algorithm";
+let videoHover = "autoplay";
+
+document.write("<h3> Name : " + userName2 +  vegetaDialogue +"</h3>")
+
+
+
+//console.log ("My name is:" + username + " and my age is: " + age);
+//console.log("the constant value is:" + tax);
+
+
+document.write("<p> Name :"+ username +" and the age is :" + age +"</p>")
+//the $ money sign is used to tel;l the compilier we are using Html symbols*
+document.write(`
+<p> Name: ${username}</p> 
+<p>age is: ${age}</p>
+`)

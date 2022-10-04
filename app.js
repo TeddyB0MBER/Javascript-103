ecd620081244424fd09bbd1a757ff531852eb0c5
Teddy <106557123+TeddyB0MBER@users.noreply.github.com>
2022-10-04 15:32:43 -0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-function addTask() {
-    let inputTask = document.getElementById('inputTask');
-    console.log(inputTask.value); // place .value behind input to display text placed in the search bars
-    
-    let ul = document.getElementById('taskList');
-    ul.innerHTML+= `<li>${inputTask.value}</li>`; // the + sign prior to the equal will allow multiple inputs to be displayed without replacing each other. there must be no space between the + sign and the equal.
-    inputTask.value = '';
-}
